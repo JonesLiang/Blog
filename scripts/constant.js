5d839c91c953ff545d1d21450e87bbd962fe2235
梁兴虎 <liangxinghu_liam@outlook.com>
2022-04-21 17:34:12 +0800

    initial commit

---
+++ scripts/constant.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+const isDev = process.env.NODE_ENV !== 'prodruction'
+const SERVER_HOST = '127.0.0.1'
+const SERVER_PORT = 9000
+const PROJECT_PATH = path.resolve(__dirname, '../')
+const PROJECT_NAME = path.parse(PROJECT_PATH).name
+
+module.exports = {
+  PROJECT_PATH,
+  PROJECT_NAME,
+  isDev,
+  SERVER_HOST,
+  SERVER_PORT,
+}
